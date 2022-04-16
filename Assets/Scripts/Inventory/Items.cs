5b488a1510d1f216c6a319da8488d0ee09b6a743
Eliott <eliott.begon@epita.fr>
2022-04-16 06:08:57 +0200

    initialisation map + scripts

---
+++ Assets/Scripts/Inventory/Items.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+[CreateAssetMenu(fileName = "item", menuName = "ScriptableObject/Item", order = 1)]
+
+public class Items : ScriptableObject
+{
+    public string title;
+    public string description;
+    public Sprite icon;
+    public int amount, goldToGive, amountToHeal;
+    public bool isStackable
+
+    [System.Serializable]
+
+    public enum Type
+    {
+        Quest, Consommable, Commun
+    }
+
+    public Type type;
+
+}*/
